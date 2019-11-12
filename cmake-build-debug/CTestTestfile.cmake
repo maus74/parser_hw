d778f897da0c45c1b9c62eadcb7e3662c1fa6404
@Maus_74 <nikita.potapovich@mail.ru>
2019-11-13 02:47:03 +0300

    cov

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/maus/homeworks/iu10/lab-01-parser
+# Build directory: /home/maus/homeworks/iu10/lab-01-parser/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/home/maus/homeworks/iu10/lab-01-parser/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/maus/homeworks/iu10/lab-01-parser/CMakeLists.txt;53;add_test;/home/maus/homeworks/iu10/lab-01-parser/CMakeLists.txt;0;")
