ccd982b1067a8737490a36f680cce0abb81b218d
maus74 <nikita_despair@mail.ru>
2019-11-14 19:16:59 +0300

    Create operators.cpp

---
+++ sources/operators.cpp
@@ -0,0 +1,51 @@
+#include"header.hpp"
+
+any& Json::operator[](const string& key)
+{
+	if (is_object())
+	{
+		auto& map = any_cast<std::map<string, any>&>(data);
+		return map[key];
+	}
+
+}
+
+std::any& Json::operator[](int index)
+{
+	if (is_array())
+	{
+		auto& vector = std::any_cast<std::vector<std::any>&>(data);
+		return vector[index];
+	}
+	else {
+		throw std::logic_error("Error");
+	}
+}
+/*
+const any& Json::operator[](const string& key) const
+{
+	if (is_object())
+	{
+		auto test = data;
+		auto& mmap = any_cast<map<string, any>&>(test);
+		return mmap[key];
+	}
+	else {
+		throw std::logic_error("Error");
+	}
+}
+*/
+/*
+const std::any& Json::operator[](int index) const
+{
+	if (is_array())
+	{
+		auto test = data;
+		auto& vector = std::any_cast<std::vector<std::any>&>(test);
+		return vector[index];
+	}
+	else {
+		throw std::logic_error("Error!!!");
+	}
+}
+*/
